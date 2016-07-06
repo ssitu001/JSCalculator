8ec559a9ccf297607de49e0bed1a1dfe93ead898
Simon Situ <ssitu001@gmail.com>
2016-07-05 22:48:26 -0700

    CE, operators, and equalworking

--- app.js
+++ app.js
@@ -6,22 +6,49 @@
   //global vars
   var valChainer = '';
    
-  $('#allClear').on('click', function() {                  //console.log("allcleared!");
-     result = '';
-     displayValue(0);
+  $('#allClear').on('click', function() {                  
+    zeroOutdisplayValue()
+  });
+
+  $('#cancelEntry').on('click', function() {
+    valChainer = valChainer.slice(0, -1);                  
+    displayValue(valChainer);
   });
 
   $('.number').on('click', function() {
     var number = $(this).val();//get value
-    var sum = valChainer += number; //chain the values
-    //console.log(sum);
-    displayValue(sum);
+    var chained = valChainer += number; //chain the values
+    
+    displayValue(chained);
   }); 
+
+  //Operator
+  $('.operator').on('click', function() {
+    //zeroOutdisplayValue();
+    var operator = $(this).val();
+    var chained = valChainer += operator;
+    displayValue(chained);
+
+  });
+
+  $('#equals').on('click', function() {
+    var evaluation = eval(valChainer);
+    valChainer = evaluation.toString();
+    displayValue(evaluation);
+  });
+
    
-   function displayValue(num) {
-     document.getElementById('input').value = num;
-    }
+  function displayValue(num) {
+    document.getElementById('input').value = num;
+  }
    
+  function zeroOutdisplayValue() {
+    displayValue(0);
+    valChainer = '';
+  } 
+  // function operate(operand) {
+
+  // } 
 
    
    
